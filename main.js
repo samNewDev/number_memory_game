4b5346c4dc539a64cf5645e49cc15a009941355c
Sxayavong <Sxayavong@edevice.com>
2020-09-23 22:46:45 +0200

    start function ongoing

--- main.js
+++ main.js
@@ -7,10 +7,20 @@ $(document).ready(function() {
     }
     function flipCard(){
         $('.flip-container .flipper').click(function() {
+            //let val = $(this).attr("data-value")
+            console.log($(this))
             $(this).closest('.flip-container').toggleClass('hover');
             $(this).css('transform, rotateY(180deg)');
+
         });
     }
+    function start() {
+        $('#start').click(function () {
+            $('.container').attr('style', 'display:flex')
+            $('.selection').hide()
+        })
+    }
+    start()
     getDifficulty()
     flipCard()
 });
