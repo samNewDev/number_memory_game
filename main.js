417a6455d40ec828222eab90adf83e86e4b553f5
Sxayavong <Sxayavong@edevice.com>
2020-09-24 11:12:19 +0200

    webcomponent ongoing

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 $(document).ready(function() {
+
+    const template = document.createElement('template');
+    const container = document.getElementById('container');
+
+    template.
+
+    class MemoryCard extends HTMLElement{
+        constructor() {
+            super();
+
+            this._shadowRoot = this.attachShadow({ mode: 'open' });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+    }
+
+    window.customElements.define('my-card', MemoryCard);
+
+
+
+
+
+
+    // recupere la valeur selectionee dans le select
     function getDifficulty() {
         $('#submit').click(function () {
             var difficulty = $('#difficulty_choice').val();
             console.log(difficulty);
         })
     }
+    /*
+    **
+    *  $this est le contexte ou est appele le code, dans notre cas c'est la 'card' qui a ete cliauee qui l'appelle
+    */
     function flipCard(){
         $('.flip-container .flipper').click(function() {
             //let val = $(this).attr("data-value")
@@ -14,6 +41,9 @@ $(document).ready(function() {
 
         });
     }
+
+    // pour l'instant cette fonction ne fait que faire disparaitre le "pseudo-formulaire"
+    // et faire apparaitre les cartes quand le button start est actionne
     function start() {
         $('#start').click(function () {
             $('.container').attr('style', 'display:flex')
